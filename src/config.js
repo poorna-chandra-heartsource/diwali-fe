0d540bf386d0b561d271233714cd89cbab203b5c
poornagollakoti <chandra.gollakoti@gmail.com>
2024-10-18 23:51:27 +0530

    updated config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    backendURL: process.env.BACKEND_URL || 'http://4.255.113.175:3000'
+    backendURL: process.env.BACKEND_URL || 'http://52.190.10.71:3000'
   };
   
 export default config;
